e562db4fdd81be1e0b9f7e76c4b17e49f691fcce
unknown <manikandan.jn@fibonalabs.com>
2022-08-11 18:30:50 +0530

    Refector to map

--- src/Components/ModalComp.tsx
+++ src/Components/ModalComp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Button, Modal, Row, Col } from 'antd';
-import { AreaChartOutlined } from '@ant-design/icons'
+import { Button, Modal, Row} from 'antd';
+import Content from './Content'
+import contentDetails from './FetchData';
 
 //const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
 
@@ -9,10 +10,6 @@ const ModalComp = () => {
 
     const showModal = () => {
         setIsModalVisible(true);
-    };
-
-    const handleOk = () => {
-        setIsModalVisible(false);
     };
 
     const handleCancel = () => {
@@ -24,108 +21,19 @@ const ModalComp = () => {
         <Button type="primary" onClick={showModal}>
             Click
         </Button>
-        <Modal title="8513 - B Statistics" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={false}>
+        <Modal title="8513 - B Statistics" visible={isModalVisible} onCancel={handleCancel} footer={false}>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="first_rows">
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <div className='col_content'>
-                                <h1>Nunam<sub></sub></h1>
-                            </div>
-                        <p>Battery Provider</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                            <div className='col_content'>
-                                <h1>60<sub>%</sub></h1>
-                            </div>
-                        <p>SOC</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                            <div className='col_content'>
-                                <h1>5<sub>h</sub></h1>
-                            </div>
-                        <p>Charge</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <div className='col_content'>
-                                <h1>37<sub>%</sub></h1>
-                            </div>
-                        <p>Discharge</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <div className='col_content'>
-                                <h1>10<sub>V</sub></h1>
-                            </div>
-                        <p>Voltage</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>0</h1>
-                        <p>Alarms</p>
-                    </div>
-                </Col>
+                {contentDetails.map((contentDisp:any) => {
+                    return(
+                        <Content 
+                            id={contentDisp.id}
+                            title={contentDisp.title}
+                            subtitle={contentDisp.subtitle}
+                            description={contentDisp.description}
+                        />
+                    )
+                })}
             </Row>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="first_rows">
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>5.21</h1>
-                        <p>FCE</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>0</h1>
-                        <p>Violations</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <div className='col_content'>
-                                <h1>48.54<sub>kwh</sub></h1>
-                            </div>
-                        <p>Energy Delivered</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>29:40</h1>
-                        <p>Hours Used</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>N/A</h1>
-                        <p>SOH</p>
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <div className='cols'>
-                        <AreaChartOutlined className='iconstyle'/>
-                        <h1>24:00</h1>
-                        <p>Hours of Charge</p>
-                    </div>
-                </Col>
-            </Row>
-            
         </Modal>
       </div>
   )
