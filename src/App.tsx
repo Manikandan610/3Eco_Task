2f721f96072751de9496d27b37178dc80cdd27fa
unknown <manikandan.jn@fibonalabs.com>
2022-08-16 19:22:02 +0530

    DatePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
-import ModalComp from './Components/ModalComp';
+//import './App.css';
+//import ModalComp from './Components/ModalComp';
 import 'antd/dist/antd.css';
+import Filter from './Filter/Filter'
+import './Filter/Filter.css'
 
 
 function App() {
   return (
-      <ModalComp />
+      // <ModalComp />
+      <Filter />
     );
 }
 
---
+++ src/Filter/Filter.tsx
@@ -0,0 +1,81 @@
+import React, { MouseEventHandler, useState } from "react";
+import type { RadioChangeEvent } from "antd";
+import { Input, Modal, Button, Radio, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import DateComp from "./DateComp";
+
+const { Search } = Input;
+const onSearch = (value: string) => console.log(value);
+
+const Filter = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [value, setValue] = useState("Today");
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setValue("Today");
+    setIsModalVisible(false);
+  };
+
+  //Radio
+  const onChange = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
+  const handleOk = (e: any) => {
+    alert(value);
+    console.log("radio checked", value);
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className="filter">
+      <Search
+        placeholder="input search text"
+        onSearch={onSearch}
+        style={{ width: 200 }}
+      />
+      <div className="btn-dropdown">
+        Date
+        <Button type="link" block className="dropdown" onClick={showModal}>
+          {value} <DownOutlined />
+        </Button>
+        <Modal
+          style={{ top: 30, left: 380 }}
+          width={300}
+          title="Filter by Date"
+          visible={isModalVisible}
+          footer={false}
+        >
+          <Radio.Group onChange={onChange} value={value}>
+            <Space direction="vertical" className="radio-gap">
+              <Radio value={"Today"}>Today</Radio>
+              <Radio value={"Last 7 days"}>Last 7 days</Radio>
+              <Radio value={"Last 30 days"}>Last 30 days</Radio>
+              <Radio value={"Custom"}>Custom</Radio>
+              {value === "Custom" ? <DateComp /> : null}
+            </Space>
+          </Radio.Group>
+          <div className="main-btn">
+            <Button className="first-btn" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button className="second-btn" onClick={handleOk}>
+              Apply
+            </Button>
+          </div>
+        </Modal>
+        Location
+        <Button type="link" block className="dropdown">
+          Link <DownOutlined />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
