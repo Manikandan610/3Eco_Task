06c1a10be456c0eebc5f010abf901be203db7bb3
unknown <manikandan.jn@fibonalabs.com>
2022-08-17 17:44:57 +0530

    Removed Comment Lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-//import './App.css';
-//import ModalComp from './Components/ModalComp';
 import 'antd/dist/antd.css';
 import Filter from './Filter/Filter'
 import './Filter/Filter.css'
 
-
 function App() {
   return (
-      // <ModalComp />
       <Filter />
     );
 }
