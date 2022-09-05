1c2b5a8ed935274f849c405882e2c13e5783d1d4
unknown <manikandan.jn@fibonalabs.com>
2022-09-05 18:11:30 +0530

    JS Object

--- src/JSChallenges/jsArrays.js
+++ src/JSChallenges/jsArrays.js
@@ -301,9 +301,7 @@ Expected
 // Merge both arrays and remove duplicate values
 // Sort the merge result in ascending order
 // Return the resulting array
-function myFunction(
-a, b
-)
+function myFunction(a, b)
 {
 let c=a.concat(b)
  var removeDuplicate= [];
